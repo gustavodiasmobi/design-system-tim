8c32a1d55910b55614c4b96d7336e2ba16b4fc5f
gustavo <gustavo.dias@mobi2buy.com>
2023-06-28 17:03:30 -0300

    Initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,17 @@
+/** @type { import('@storybook/html').Preview } */
+
+import "../styles/scss/pages/main.css";
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ stories/layouts/Header/Header.stories.js
@@ -0,0 +1,8 @@
+import Header from "./Header.html";
+
+export default {
+  title: "Layouts/Header",
+  tags: ["autodocs"],
+};
+
+export const header = () => Header;
